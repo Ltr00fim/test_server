f33bfde811c18fceb950fc4e131dc629d42dcdbf
lera <120723754+Ltr00fim@users.noreply.github.com>
2023-04-03 09:26:41 +0300

    Update implemented.py

--- implemented.py
+++ implemented.py
@@ -12,7 +12,6 @@ from service.genre import GenreService
 from dao.user import UserDao
 from service.user import UserService
 
-from service.auth import AuthService
 
 movie_dao = MovieDao(db.session)
 genre_dao = GenreDao(db.session)
@@ -23,4 +22,3 @@ movie_service = MovieService(dao=movie_dao)
 genre_service = GenreService(dao=genre_dao)
 director_service = DirectorService(dao=director_dao)
 user_service = UserService(dao=user_dao)
-auth_service = AuthService(user_service)
